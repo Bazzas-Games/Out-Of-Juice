e423505beb412d33da11c204e3aaa9eedeaceea1
Kownur <62537455+Kownur@users.noreply.github.com>
2020-04-18 22:38:23 +1000

    Scenes Added
    Main Menu
    Pause Menu
    - UI created.
    - PauseMenuController created & scripted.

---
+++ Assets/Scripts/LaserController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    private List<GameObject> beams = new List<GameObject>();
+    public GameObject[] laser;
+    public bool isPowered = true;
+
+    // Time when the timer started.
+    private float startTime;
+    private float elapsedTime;
+
+    public float targetTime = 2f;
+
+    private void Start()
+    {
+
+        startTime = Time.time;
+        isPowered = true;
+        foreach (GameObject l in laser)
+        {
+            beams.Add(l.GetComponentInChildren<EnviroHazard>().gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        elapsedTime = (Time.time - startTime);
+        if (elapsedTime >= targetTime)
+        {
+            PowerToggle();
+        }
+        if (isPowered == true)
+        {
+           foreach(GameObject b in beams)
+            {
+                b.SetActive(true);
+            }
+        }
+        else
+        {
+            foreach(GameObject b in beams)
+            {
+               b.SetActive(false);
+            }
+            
+        }
+
+    }
+    void PowerToggle()
+    {
+        isPowered = !isPowered;
+        startTime = Time.time;
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+
+    [SerializeField] private bool isPaused;
+
+    
+    private void Update()
+    {
+        // When "Escape" is pressed, Resume game if Paused.
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+        
+        if (isPaused)
+        {
+            ActivateMenu();
+        }
+        else
+        {
+            DeactivateMenu();
+        }
+    }
+
+    void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void DeactivateMenu()
+    {
+        Time.timeScale = 1;
+        pauseMenuUI.SetActive(false);
+        isPaused = false;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
