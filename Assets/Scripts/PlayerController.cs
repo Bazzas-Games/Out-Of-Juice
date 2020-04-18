526f7fc06aab929aea49379985c329f2167506e8
Mungo_ <bailey.gibbons2018@gmail.com>
2020-04-18 16:42:07 +1000

    Started working on grapple

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,48 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    
     public bool isGrounded = false;
     public bool isTouchingWall = false;
-    public Vector2 inputVector = Vector2.zero;
+    public bool isGrappling = false;
+    public float groundSpeed = 5f;
+    public float airSpeed = 2f;
+
+    public float grappleSpeed = 5f;
+    public float grappleRange = 5f;
+
+    private Vector2 inputVector = Vector2.zero;
+    private Vector2 grappleVector = Vector2.zero;
     private List<Vector2> contactVectors = new List<Vector2>();
+    private Rigidbody2D rb;
+    private RaycastHit2D grapplePoint;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
         InputPoll();
+        if (isGrounded) AccelerateGround();
+        else AccelerateAir();
     }
+
 
     void InputPoll()
     {
         inputVector.x = Input.GetAxisRaw("Horizontal");
         inputVector.y = Input.GetAxisRaw("Vertical");
-        inputVector.Normalize();
+        if(Input.GetAxisRaw("Fire1") == 1 && !isGrappling)
+        {
+            Grapple(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        }
+        if(Input.GetAxisRaw("Fire1") == 0 && isGrappling)
+        {
+            isGrappling = false;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -31,9 +58,36 @@ public class PlayerController : MonoBehaviour
             if (!contactVectors.Contains(contactVector)) contactVectors.Add(contactVector);
         }
     }
-    void Kill()
+    public void Kill()
     {
         Debug.Log("Player is ded");
+    }
+
+    void AccelerateGround()
+    {
+        rb.AddForce(new Vector2(inputVector.x * groundSpeed, 0));
+    }
+
+    void AccelerateAir()
+    {
+        rb.AddForce(new Vector2(inputVector.x * airSpeed, 0));
+    }
+
+    void AccelerateGrapple()
+    {
+        rb.AddForce(grappleVector * grappleSpeed);
+        rb.AddForce(inputVector.normalized * airSpeed);
+    }
+    
+    void Grapple(Vector2 direction)
+    {
+        grapplePoint = Physics2D.Raycast(transform.position, direction - (Vector2)transform.position, grappleRange, LayerMask.GetMask("Environment"));
+        Debug.DrawRay(transform.position, (direction - (Vector2)transform.position).normalized * grappleRange);
+        if (grapplePoint.collider != null)
+        {
+            isGrappling = true;
+            Debug.Log("hit "+ grapplePoint.collider.name);
+        }
     }
 
 
