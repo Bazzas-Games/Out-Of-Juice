43419654f85c2c620e961d42cc5a4c861baff776
Kownur <62537455+Kownur@users.noreply.github.com>
2020-04-18 15:06:03 +1000

    Font

---
+++ Assets/Scripts/EnviroHazard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnviroHazard : MonoBehaviour
+
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Hazard"))
+        {
+            GetComponent<PlayerController>().Kill();
+        }
+    }
+}
