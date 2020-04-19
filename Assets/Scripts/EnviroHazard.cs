ad5b63ac695553b51cd573a31d40e75443013434
Kownur <62537455+Kownur@users.noreply.github.com>
2020-04-19 16:23:57 +1000

    Parallax BG created

--- Assets/Scripts/EnviroHazard.cs
+++ Assets/Scripts/EnviroHazard.cs
@@ -3,18 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnviroHazard : MonoBehaviour
-
 {
+    
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Hazard"))
         {
             GetComponent<PlayerController>().Kill();
+            Debug.Log("Colliding");
         }
-    }
-
-    public void Death()
-    {
-        Debug.Log("Am dead");
     }
 }
