20522149ea8faf213083bb0dc12ba7f1d81a3621
Mungo_ <bailey.gibbons2018@gmail.com>
2020-04-20 21:02:17 +1000

    changed camera to true 16:9 resolution

---
+++ Assets/Scripts/ScreenController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenController : MonoBehaviour
+{
+    public Vector2 referenceResolution = new Vector2(512, 288);
+    public Camera camera;
+    private GameObject player;
+
+
+    void Start()
+    {
+        if (camera == null) camera = Camera.main;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
