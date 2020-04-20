3bb37f47f3b58adbc084e81d979d5eb9e57fb7ca
Mungo_ <bailey.gibbons2018@gmail.com>
2020-04-20 23:15:35 +1000

    ScreenController works

--- Assets/Scripts/ScreenController.cs
+++ Assets/Scripts/ScreenController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class ScreenController : MonoBehaviour
 {
     public Vector2 referenceResolution = new Vector2(512, 288);
-    public Vector2 cameraOffset = new Vector2(-16, -9);
+    public Vector3 cameraOffset = new Vector3(-16, -9, 0);
     public int pixelsPerUnit = 16;
     public Camera sceneCamera;
 
@@ -17,7 +17,6 @@ public class ScreenController : MonoBehaviour
 
     private GameObject player;
 
-
     void Start() {
         if (sceneCamera == null) sceneCamera = Camera.main;
         player = GameObject.FindGameObjectWithTag("Player");
@@ -25,20 +24,23 @@ public class ScreenController : MonoBehaviour
     }
 
     void Update() {
-        
+        Vector3 offsetPlayerPos = player.transform.position + cameraOffset;
+        sceneCamera.transform.position = new Vector3(Mathf.Ceil((offsetPlayerPos.x/unitsPerScreen.x)) * unitsPerScreen.x, Mathf.Ceil(offsetPlayerPos.y/unitsPerScreen.y) * unitsPerScreen.y, -20);
     }
+
+
     void OnDrawGizmos() {
 #if UNITY_EDITOR
         Gizmos.color = Color.grey;
-        Vector2 max = new Vector2(numberOfScreens.x * unitsPerScreen.x, numberOfScreens.y * unitsPerScreen.y) + cameraOffset;
+        Vector2 max = new Vector3(numberOfScreens.x * unitsPerScreen.x, numberOfScreens.y * unitsPerScreen.y, 0) + cameraOffset;
         for (int i = (int)(-numberOfScreens.x/2); i < numberOfScreens.x/2; i++) {
-            Vector3 start = new Vector3(i * unitsPerScreen.x, max.y/2, 0) + (Vector3)cameraOffset;
-            Vector3 end = new Vector3(i * unitsPerScreen.x, -max.y/2, 0) + (Vector3)cameraOffset;
+            Vector3 start = new Vector3(i * unitsPerScreen.x, max.y/2, 0) + cameraOffset;
+            Vector3 end = new Vector3(i * unitsPerScreen.x, -max.y/2, 0) + cameraOffset;
             Gizmos.DrawLine(start, end);
         }
         for(int i = (int)(-numberOfScreens.y / 2); i < numberOfScreens.y / 2; i++) {
-            Vector3 start = new Vector3(-max.x/2, i*unitsPerScreen.y, 0) + (Vector3)cameraOffset;
-            Vector3 end = new Vector3(max.x/2, i*unitsPerScreen.y, 0) + (Vector3)cameraOffset;
+            Vector3 start = new Vector3(-max.x/2, i*unitsPerScreen.y, 0) + cameraOffset;
+            Vector3 end = new Vector3(max.x/2, i*unitsPerScreen.y, 0) + cameraOffset;
             Gizmos.DrawLine(start, end);
         }
 #endif
