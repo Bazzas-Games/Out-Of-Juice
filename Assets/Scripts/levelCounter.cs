2283f055b4f5757504865d2937d163b8a8d119dd
Kownur <62537455+Kownur@users.noreply.github.com>
2020-05-08 16:29:31 +1000

    Created StageCounter
    Created script that displays Stage Level at beginning of each new scene.
    Created overlay canvas and text that displays as overlay.
    Made it a prefab and added it to Template Scene for easier implimentation.

---
+++ Assets/Scripts/levelCounter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+//public TextMeshProGUI Level = SceneManager.GetActiveScene().buildIndex.ToString("0");
+
+public class levelCounter : MonoBehaviour
+{
+    public TextMeshProUGUI levelText;
+    public int buildIndex;
+    public GameObject stageCounter;
+    public float sec = 12f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        stageCounter.SetActive (true);
+
+        buildIndex = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log(SceneManager.GetActiveScene().buildIndex + 1);
+        levelText.text = "Stage " + buildIndex.ToString("0");
+
+        //Start the coroutine DeactivationRoutine().
+        StartCoroutine(DeactivationRoutine());
+    }
+
+    public void levelUpdate()
+    {
+        Debug.Log("Triggered");
+        levelText.text = "Stage " + buildIndex.ToString("0");
+        Debug.Log(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private IEnumerator DeactivationRoutine()
+    {
+        //Wait for sec.
+        yield return new WaitForSeconds(sec);
+        stageCounter.SetActive (false);
+    }
+
+}
